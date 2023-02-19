610134a9f3458834411a8eae58ade7c8dfb4bc8a
pasupol.bunsaen <niawkung@gmail.com>
2023-02-19 19:43:00 +0700

    fix: resolve issue with JWT authentication not working correctly

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,5 @@
 // user.controller.ts
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,8 +7,11 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  async createUser(@Body() body: { user: string; password: string }) {
-    const user = await this.userService.createUser(body.user, body.password);
+  async createUser(@Body() body: { username: string; password: string }) {
+    const user = await this.userService.createUser(
+      body.username,
+      body.password,
+    );
     return { user };
   }
 }
